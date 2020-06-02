18eb1f969ac2ea2fd07d3689aabb050e0e6fd547
Shafi Akinropo <sakinropo@gmail.com>
2020-06-02 19:51:41 +0100

    Making script to get current page url dynamically

--- index.php
+++ index.php
@@ -1,13 +1,10 @@
-<?php 
-  $path = dirname(__FILE__). '/scripts/' ;
-	
-	//Get a list of file paths using the glob function.
-	  $allFilesList= glob('scripts/*');
-	  //Loop through the array that glob returned.
-	  foreach($allFilesList as $filename){
-	    //Simply print them out onto the screen.
-	    echo file_get_contents('http://localhost/project/' . $filename);
-
-	  }
-
- ?>
+<?php
+    $url = $_SERVER['REQUEST_URI']. '/scripts/';
+    
+    //Get a list of file paths using the glob function.
+      $allFilesList= glob('scripts/*');
+      //Loop through the array that glob returned.
+      foreach ($allFilesList as $filename) {
+          //Simply print them out onto the screen.
+          echo file_get_contents($url . $filename);
+      }
